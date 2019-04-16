7d29e008cd52766ca49976994e029dd67c916cc0
msghf1366 <msghf1366@gmail.com>
2019-04-16 14:03:00 +0430

    implements method find,remove,removeall

--- mylinklist/MyLinkList.java
+++ mylinklist/MyLinkList.java
@@ -1,40 +1,85 @@
 package mylinklist;
 
 public class MyLinkList {
+    static Node pointer;
+    static Node prev;
     Node first;
 
     public void add(int o) {
 
-        Node new_Node=new Node(o);
-        new_Node.next=null;
+        Node new_Node = new Node(o);
+        new_Node.next = null;
 
-        if(this.first==null){
-            this.first=new_Node;
-        }else{
-            Node last=this.first;
-            while (last.next!=null){
-                last=last.next;
+        if (this.first == null) {
+            this.first = new_Node;
+        } else {
+            Node last = this.first;
+            while (last.next != null) {
+                last = last.next;
             }
-            last.next=new_Node;
+            last.next = new_Node;
         }
 
 
-
-    }
-    public void remove(int o){
-
-    }
-    public  void isEmpty(int o){
-
     }
 
-    public void printLinkList(MyLinkList list){
-
-        Node pointer=list.first;
-        while (pointer !=null){
-            System.out.print(pointer.value+" ");
+    public void removeAll(){
+        pointer=first;
+        while (pointer.next!=null){
+            first=null;
+            prev=pointer;
             pointer=pointer.next;
+            prev.next=null;
+
         }
+    }
+
+    public void remove(int o) {
+        if (this.find(o)) {
+            if (pointer==first){
+                first=pointer.next;
+            }else if(pointer.next !=first && pointer.next!=null){
+                prev.next=pointer.next;
+            }else if(pointer.next==null){
+                pointer=null;
+            }
+
+        }
+
+    }
+
+    public boolean find(int o) {
+        pointer = this.first;
+        while (pointer != null) {
+            if (pointer.value == o) {
+                return true;
+            } else {
+                prev=pointer;
+               pointer = pointer.next;
+            }
+        }
+        return false;
+    }
+
+
+    public boolean isEmpty() {
+        if (this.first == null) {
+            System.out.println("List is Empty");
+            return true;
+        } else return false;
+
+    }
+
+    public void printLinkList(MyLinkList list) {
+
+        pointer = list.first;
+        System.out.print("{");
+        while (pointer != null) {
+            System.out.print(pointer.value + ",");
+            pointer = pointer.next;
+        }
+        System.out.print("}");
+        System.out.println();
 
     }
 
@@ -48,6 +93,10 @@ public class MyLinkList {
 
         }
 
+        @Override
+        public String toString() {
+            return value+"";
+        }
     }
 
 }
