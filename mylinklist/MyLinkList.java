5e71d4da0b817da17126e9d98764d7e826efe5b9
msghf1366 <msghf1366@gmail.com>
2019-04-16 12:30:18 +0430

    create class node create method add,remove,print,isEmpty impalement method add

--- mylinklist/MyLinkList.java
+++ mylinklist/MyLinkList.java
@@ -1,4 +1,53 @@
 package mylinklist;
 
 public class MyLinkList {
+    Node first;
+
+    public void add(int o) {
+
+        Node new_Node=new Node(o);
+        new_Node.next=null;
+
+        if(this.first==null){
+            this.first=new_Node;
+        }else{
+            Node last=this.first;
+            while (last.next!=null){
+                last=last.next;
+            }
+            last.next=new_Node;
+        }
+
+
+
+    }
+    public void remove(int o){
+
+    }
+    public  void isEmpty(int o){
+
+    }
+
+    public void printLinkList(MyLinkList list){
+
+        Node pointer=list.first;
+        while (pointer !=null){
+            System.out.print(pointer.value+" ");
+            pointer=pointer.next;
+        }
+
+    }
+
+    static class Node {
+        int value;
+        Node next;
+
+        Node(int value) {
+            this.value = value;
+            next = null;
+
+        }
+
+    }
+
 }
